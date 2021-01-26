53934c252fe4871d5aefeefe76d4892289327a8d
Kalvin dela Raga <kalvin_delaraga@ligph.com>
2021-01-26 17:52:41 +0800

    add string resources

---
+++ ChatApp/resources/values/BDStrings.swift
@@ -0,0 +1,17 @@
+//
+//  StringValue.swift
+//  ChatApp
+//
+//  Created by Kalvin Dela Raga on 1/26/21.
+//
+
+import Foundation
+
+class BDStrings {
+    
+    static let actionSignUp = "Sign Up"
+    
+    static let actionLogin = "Login"
+    
+    static let displayTerms = "By signing up, you agree to the Terms of  Service and Privacy Policy, including Cookie Use. Others will be able to find you by searching for your email address and phone number when provided."
+}
