3535c7c701fe55e401fcd6ad7de19d68cfbc7d9b
Kalvin dela Raga <kalvin_delaraga@ligph.com>
2021-01-26 17:52:22 +0800

    add custom views

---
+++ ChatApp/ui/common/DefaultTextField.swift
@@ -0,0 +1,40 @@
+//
+//  DefaultTextField.swift
+//  ChatApp
+//
+//  Created by Kalvin Dela Raga on 1/26/21.
+//
+
+import UIKit
+
+class DefaultTextField: UITextField {
+    
+    private let padding = UIEdgeInsets(top: 12.0, left: 8.0, bottom: 12.0, right: 8.0)
+    
+    convenience init() {
+        self.init(frame: .zero)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init?(coder:) has not been implemented")
+    }
+    
+    private func setupLayout() {
+        backgroundColor = nil
+        borderStyle = .roundedRect
+        layer.cornerRadius = 8.0
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return super.textRect(forBounds: bounds.inset(by: padding))
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return super.editingRect(forBounds: bounds.inset(by: padding))
+    }
+}
---
+++ ChatApp/ui/common/SecondaryButton.swift
@@ -0,0 +1,53 @@
+//
+//  SecondaryButton.swift
+//  ChatApp
+//
+//  Created by Kalvin Dela Raga on 1/26/21.
+//
+
+import UIKit
+
+class SecondaryButton: UIButton {
+    
+    private var title: String = ""
+    
+    init(title: String) {
+        self.title = title
+        super.init(frame: .zero)
+        setupLayout()
+    }
+    
+    convenience init() {
+        self.init(frame: .zero)
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init?(coder:) has not been implemented")
+    }
+    
+    private func setupLayout() {
+        backgroundColor = .white
+        
+        layer.borderWidth = 0.0
+        
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.boldSystemFont(ofSize: 16),
+            .foregroundColor: UIColor.black,
+            .underlineStyle: NSUnderlineStyle.single.rawValue
+        ]
+        let attributedString = NSMutableAttributedString(string: title, attributes: attributes)
+        setAttributedTitle(attributedString, for: .normal)
+    }
+    
+    func setEnabled(_ flag: Bool) {
+        isEnabled = flag
+        
+        if isEnabled {
+            setupLayout()
+        }
+    }
+}
