cabb303b866f9c0479ef7a409f5fa843ec1d924b
Kalvin dela Raga <kalvin_delaraga@ligph.com>
2021-01-27 15:20:53 +0800

    use navigator instead of an unnecessary viewController

--- ChatApp/AppDelegate.swift
+++ ChatApp/AppDelegate.swift
@@ -6,25 +6,28 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        
         if #available(iOS 13.0, *) {
-
-        } else {
-            self.window = UIWindow(frame: UIScreen.main.bounds)
-            self.window?.rootViewController = JanusViewController()
-            self.window?.makeKeyAndVisible()
+            
+        } else if let window = self.window {
+            let janus = Janus(window: window)
+            janus.start()
         }
-
+        
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
     @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
@@ -32,7 +35,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     @available(iOS 13.0, *)
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
