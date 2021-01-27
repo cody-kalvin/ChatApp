16c70cf4d4d7c8da3ee2b7341f3bca9235f4110c
Kalvin dela Raga <kalvin_delaraga@ligph.com>
2021-01-27 18:53:49 +0800

    fix missing delegate

--- ChatApp/janus/Janus+Navigation.swift
+++ ChatApp/janus/Janus+Navigation.swift
@@ -12,6 +12,7 @@ extension Janus {
     func gotoSignUp() {
         let viewModel = SignUpViewModel()
         let viewController = SignUpViewController(viewModel: viewModel)
+        viewController.delegate = self
         self.window.rootViewController = viewController
         self.window.makeKeyAndVisible()
     }
